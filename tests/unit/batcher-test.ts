7aff105071882b8d9ab639d0b322aa65f9213f16
Steve Calvert <scalvert@linkedin.com>
2019-07-26 13:00:37 -0700

    Adding tests for ensuring waiters

--- tests/unit/batcher-test.ts
+++ tests/unit/batcher-test.ts
@@ -1,6 +1,8 @@
 import { module, test } from 'ember-qunit';
 import { settled } from '@ember/test-helpers';
 import { readDOM, mutateDOM } from 'ember-batcher';
+import { getPendingWaiterState, ITestWaiterDebugInfo } from 'ember-test-waiters';
+import { assert } from '@ember/debug';
 
 module('Unit | Batcher', function() {
   test('it runs reads', async function(assert: Assert) {
@@ -77,5 +79,31 @@ module('Unit | Batcher', function() {
     await settled();
 
     assert.verifySteps(['First read', 'Second read', 'First write', 'Second write', 'Third write']);
+  });
+
+  test('waiter is correctly wired up for readDOM', async function(assert: Assert) {
+    function foo() {}
+
+    readDOM(foo);
+
+    let pendingWaiters = getPendingWaiterState();
+
+    assert.equal(pendingWaiters.pending, 2);
+    assert.ok((<ITestWaiterDebugInfo[]>pendingWaiters.waiters['ember-batcher readDOM waiter'])[0].stack!.indexOf('readDOM') > -1);
+
+    await settled();
+  });
+
+  test('waiter is correctly wired up for mutateDOM', async function(assert: Assert) {
+    function foo() {}
+
+    mutateDOM(foo);
+
+    let pendingWaiters = getPendingWaiterState();
+
+    assert.equal(pendingWaiters.pending, 2);
+    assert.ok((<ITestWaiterDebugInfo[]>pendingWaiters.waiters['ember-batcher mutateDOM waiter'])[0].stack!.indexOf('mutateDOM') > -1);
+
+    await settled();
   });
 });
