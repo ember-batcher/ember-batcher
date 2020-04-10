e7d9a24c60a721588507b3b3f475299a932fad8f
Steve Calvert <steve.calvert@gmail.com>
2020-04-10 11:00:48 -0700

    Enabling support for detecting if the batcher is running in a ba… (#177)
    * feat(tab support): Enabling support for detecting if the batcher is running in a background tab

--- addon/batcher.ts
+++ addon/batcher.ts
@@ -1,19 +1,57 @@
-import { buildWaiter, ITestWaiter, Token } from 'ember-test-waiters';
+import { ITestWaiter, Token, buildWaiter } from 'ember-test-waiters';
+
+import { DEBUG } from '@glimmer/env';
 
 type MaybeRequestAnimationFrame = (callback: FrameRequestCallback | Function) => number;
 type DomOperation = [Token, () => void];
 
+const IS_BROWSER = typeof window === 'object' && typeof document === 'object';
 const SCHEDULE_MACROTASK = (callback: Function) => setTimeout(callback);
-const scheduleFn: MaybeRequestAnimationFrame =
-  typeof window === 'object' && typeof window.requestAnimationFrame === 'function'
-    ? window.requestAnimationFrame
-    : SCHEDULE_MACROTASK;
 const readDOMWaiter: ITestWaiter = buildWaiter('ember-batcher: readDOM');
 const mutateDOMWaiter: ITestWaiter = buildWaiter('ember-batcher: mutateDOM');
 
 const reads: Array<DomOperation> = [];
 const mutations: Array<DomOperation> = [];
+let visibilityChange: Function = () => {};
 let running: boolean = false;
+let scheduleFnExecuted: boolean = false;
+
+const rafRace = (callback: Function) => {
+  let executeCallback = () => {
+    if (!scheduleFnExecuted) {
+      scheduleFnExecuted = true;
+      callback();
+    }
+  };
+
+  setTimeout(executeCallback, 20);
+
+  return requestAnimationFrame(executeCallback);
+};
+
+const scheduleFn: MaybeRequestAnimationFrame =
+  typeof window === 'object' && typeof window.requestAnimationFrame === 'function'
+    ? rafRace
+    : SCHEDULE_MACROTASK;
+
+if (DEBUG && IS_BROWSER) {
+  visibilityChange = (
+    hidden: boolean = document.hidden,
+    hasQueuedWork: Function = () => reads.length > 0 && mutations.length > 0
+  ) => {
+    return () => {
+      if (hidden && hasQueuedWork()) {
+        throw new Error(
+          "Your browser tab is running in the background. ember-batcher's execution is not guaranteed in this environment"
+        );
+      }
+    };
+  };
+
+  document.addEventListener('visibilitychange', visibilityChange());
+}
+
+export { visibilityChange };
 
 function run(): void {
   if (!running) {
@@ -34,6 +72,7 @@ function run(): void {
       }
 
       running = false;
+      scheduleFnExecuted = false;
 
       if (mutations.length > 0 || reads.length > 0) {
         run();
